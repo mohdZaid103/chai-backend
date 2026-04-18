6a8f8f8c7ed58006ad9aabeefd744a80ba2c5e62
mohdZaid103 <mohdzaid103@outlook.com>
2026-04-18 22:01:36 +0530

    this is a commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+//require('dotenv').config({path:'./env'})
+import connectDB from "./db/index.js";
+import dotenv from "dotenv"
+
+
+/*dotenv.config({
+    path:'./env'
+}) */
+
+connectDB()
+
+
+
+/* import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+import express from "express"
+const app = express()
+
+;(async ()=>{
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error)=>{
+            console.log("error:",error)
+            throw error;
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`app is listening on port ${process.env.PORT }`)
+        })
+    }catch(error){
+        console.error(error)
+    }
+})()
+ */
