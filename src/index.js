e6050f820e7ad39c5ae049b59bbe4e8b60d45f65
mohdZaid103 <mohdzaid103@outlook.com>
2026-04-23 14:49:30 +0530

    controllers setup

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv"
 
-
 /*dotenv.config({
     path:'./env'
 }) */
-
+import app from "./app.js";
 connectDB()
 .then(()=>{
   app.listen(process.env.PORT || 8000,()=>{
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,7 @@
 //using promises
 
 const asyncHandler=(fn)=>{
-    (req,res,next)=>{
+    return (req,res,next)=>{
         Promise.resolve(fn(req,res,next)).
         catch((error)=>next(error))
     }
