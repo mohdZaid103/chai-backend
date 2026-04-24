55b9cf457343557f72e54e7d9d41fa900ceee4d8
mohdZaid103 <mohdzaid103@outlook.com>
2026-04-25 00:51:11 +0530

    login/logout auth

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,26 @@
 import {asyncHandler} from '../utils/asyncHandler.js';
 import {ApiError} from '../utils/ApiError.js'
 import { User } from '../models/user.model.js';
+
 import { upload } from '../middlewares/multer.middleware.js';
 import { uploadOnCloudinary } from '../utils/cloudinary.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
+import { response } from 'express';
 
+const generateAccessAndRefreshTokens = async(userId)=>{
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateRefreshToken()
+        const refreshToken= user.generateAccessToken()
+        user.refreshToken = refreshToken
+        await user.save({validateBeforeSave:false})
 
+        return {accessToken,refreshToken}
+
+    } catch (error) {
+        throw new ApiError(500,"something went wrong while generating tokens")
+    }
+}
 
 /*->get user details from frontend
 ->validate user data- data not empty
@@ -91,4 +106,81 @@ const registerUser = asyncHandler(async (req, res)=>{
 
 })
 
-export {registerUser}
+//get user details from frontend
+//validate userdata 
+//check if user exists in db or not if not redirect to register
+//validate user given password and password in db
+//generate access and refresh token and save in dd
+//send cookies and response 
+const loginUser = asyncHandler(async (req,res)=>{
+    //get data from req body
+    const {email, username, password} = req.body
+    
+    if(!username || !email){
+        throw new ApiError(400, "username or email is required")
+    }
+
+    const user = await User.findOne({
+        $or:[
+            {email},
+            {username}
+        ]
+    })
+    if(!user){
+        throw new ApiError(404, "user does not exist")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+    if(!isPasswordValid){
+        throw new ApiError(401, "password incorrect")
+    }
+    const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id)
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+
+    const options ={
+        httpOnly:true,
+        secure:true
+    }
+
+    return res.
+    status(200).
+    cookie("accessToken", accessToken,options).
+    cookie("refreshToken", refreshToken, options).
+    json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedInUser, accessToken,refreshToken
+            },
+            "User logged In successfully"
+        )
+    )
+})
+
+
+const logoutUser = asyncHandler(async(req,res)=>{
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set:{
+                refreshToken:undefined
+            }
+        },
+        {
+            new:true
+        }
+    )
+    const options={
+        httpOnly:true,
+        secure:true
+    }
+    return res.
+    status(200).
+    clearCookie("accessToken", options).
+    clearCookie("refreshToken", options).
+    json(new ApiResponse(200,{},"user logged out successfully"))
+})
+
+export {registerUser, loginUser, logoutUser}
