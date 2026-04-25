91c076f9f3cfd3c4272a2deefa0b40ce921736da
mohdZaid103 <mohdzaid103@outlook.com>
2026-04-25 20:03:49 +0530

    added watchHistory endpoints

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,6 +7,7 @@ import { uploadOnCloudinary } from '../utils/cloudinary.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
 import { response } from 'express';
 import jwt from "jsonwebtoken"
+import mongoose from 'mongoose';
 
 
 
@@ -413,4 +414,50 @@ const getUserChannelProfile = asyncHandler(async(req,res)=>{
 
 })
 
-export {registerUser, loginUser, logoutUser,refreshAccessToken, changeCurrentPassword, getCurrentUser,updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile}
+const getUserWatchHistory = asyncHandler(async(req,res)=>{
+    const user = await User.aggregate([
+        {
+            $match:{
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup:{
+                from:"videos",
+                localField:"watchHistory",
+                foreignField: "_id",
+                as:"watchHistory",
+                pipeline:[
+                    {
+                        $lookup:{
+                            from:"users",
+                            localField:"owner",
+                            foreignField:"_id",
+                            as:"owner",
+                            pipeline:[
+                                {
+                                    $project:{
+                                        fullName:1,
+                                        username:1,
+                                        avatar:1
+                                    }
+                                }
+                            ]
+                        }
+                    },{
+                        $addFields:{
+                            owner:{
+                                $first:"$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res.status(200).json(200, user[0].watchHistory, "watch history fetched successfully") 
+})
+
+
+export {registerUser, loginUser, logoutUser,refreshAccessToken, changeCurrentPassword, getCurrentUser,updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile,getUserWatchHistory}
