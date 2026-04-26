9bc5e210af7e2e5619ffca1e46af5c1c92fa1da5
mohdZaid103 <mohdzaid103@outlook.com>
2026-04-26 16:32:57 +0530

    healthcheck controller

---
+++ src/controllers/healthcheck.controller.js
@@ -0,0 +1,17 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+const healthcheck = asyncHandler(async (req, res) => {
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            {
+                status: "OK",
+                uptime: process.uptime(),
+                timestamp: new Date()
+            },
+            "Server is running successfully"
+        )
+    );
+});
+
+export { healthcheck }
